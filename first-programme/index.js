b9d4255725fc9da730bfa9c66e233a52c896790a
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-10-08 12:49:31 +0600

    built in type, array, tuple, union

--- first-programme/index.js
+++ first-programme/index.js
@@ -1,4 +1,45 @@
 function addNumbers(a, b) {
     console.log(a + b);
 }
-addNumbers(40, 20);
+addNumbers(40, 10);
+var userId;
+var firstName;
+var lastName;
+var fullName;
+var isActivated;
+userId = "1234";
+firstName = "John";
+lastName = "Doe";
+fullName = firstName + " " + lastName;
+isActivated = true;
+console.log("User ID: ".concat(userId, ", Full Name: ").concat(fullName, ", Is Activated: ").concat(isActivated));
+console.log(fullName.split(" "));
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+function display() {
+    console.log("Hello World");
+}
+display();
+//TypeScript Union Types
+var ID;
+ID = "123456789";
+ID = 123456789;
+ID = true;
+console.log("union userId: ".concat(ID));
+function displayUser(id, name) {
+    console.log("User ID: ".concat(id, ", User Name: ").concat(name));
+}
+displayUser("1234", "John Doe");
+//array type
+var test;
+test = ["John", "Doe", "Smith"];
+test.sort();
+console.log(test);
+var multiType;
+multiType = ["John", 123, true];
+console.log(multiType);
+// define our tuple
+var ourTuple;
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+console.log(ourTuple);
--- first-programme/index.ts
+++ first-programme/index.ts
@@ -1,4 +1,60 @@
-function addNumbers(a:number, b:number) {
+function addNumbers(a: number, b: number) {
   console.log(a + b);
 }
-addNumbers(40, 20);
+addNumbers(40, 10);
+
+let userId: string;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let isActivated: boolean;
+
+userId = "1234";
+firstName = "John";
+lastName = "Doe";
+fullName = firstName + " " + lastName;
+isActivated = true;
+console.log(
+  `User ID: ${userId}, Full Name: ${fullName}, Is Activated: ${isActivated}`
+);
+
+console.log(fullName.split(" "));
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+
+function display() : void {
+  console.log("Hello World");
+}
+display();
+
+//TypeScript Union Types
+let ID : string | number | boolean;
+ID = "123456789";
+ID = 123456789;
+ID = true;
+
+console.log(`union userId: ${ID}`);
+
+function displayUser(id: string | number, name: string) {
+  console.log(`User ID: ${id}, User Name: ${name}`);
+}
+
+displayUser("1234", "John Doe");
+
+//array type
+let test : string[];
+test = ["John", "Doe", "Smith"];
+test.sort();
+console.log(test);
+
+let multiType : (string | number | boolean)[];
+multiType = ["John", 123, true];
+console.log(multiType);
+
+
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+console.log(ourTuple);
