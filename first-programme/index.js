2682e33ebd9178e4221cb8af49b7f83b6b2721c1
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-10-08 11:27:30 +0600

    initial commit

---
+++ first-programme/index.js
@@ -0,0 +1,4 @@
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+addNumbers(40, 20);
---
+++ first-programme/index.ts
@@ -0,0 +1,4 @@
+function addNumbers(a:number, b:number) {
+  console.log(a + b);
+}
+addNumbers(40, 20);
